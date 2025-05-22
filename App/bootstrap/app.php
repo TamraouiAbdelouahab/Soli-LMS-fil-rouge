c28dc49de98b75207cdc48b18f8147bb46e9c674
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-05-22 13:07:02 +0100

    ajouter les routes

--- App/bootstrap/app.php
+++ App/bootstrap/app.php
@@ -6,7 +6,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        // web: __DIR__.'/../routes/web.php',
+         web: [
+            __DIR__.'/../routes/web.php',
+            __DIR__.'/../modules/PkgSanction/Routes/web.php',
+            __DIR__.'/../modules/PkgAbsence/Routes/web.php',
+            __DIR__.'/../modules/PkgJustificatif/Routes/web.php',
+
+         ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
