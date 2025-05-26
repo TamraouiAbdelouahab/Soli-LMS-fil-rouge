7ef72754db63832de495afbfda65d542628b67b4
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-05-26 03:52:16 +0100

    Change in import model absence

--- App/app/Models/Seance.php
+++ App/app/Models/Seance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\PkgAbsence\App\Models\Absence;
 
 class Seance extends Model
 {
